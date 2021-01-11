e5c893ed7f2a8ec2a48641599040d9155da0c355
raquellewei <weimaomao_3@hotmail.com>
2021-01-11 15:41:23 -0500

    slight modifications here and there. simple test run in main

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import ProfilingTools as pf
 import numpy as np
 import EMDUnifrac as unifrac
 from biom import Table
+import ete3
 
 def write_list_to_file(file, lst):
     with open(file, 'w') as f:
@@ -39,20 +40,24 @@ def pick_otu(node, map_dict, num):
     leaf_names = []
     for l in leaves:
         leaf_names.append(l.name)
+    filtered_leaves = list(set(list(map_dict.keys())) & set(leaf_names))
+    print('%d leaves are pickable' %len(filtered_leaves))
     # in case num is too big, return everything possible
-    if num > len(leaf_names):
-        print("%d pickable nodes" %len(leaf_names))
-        return leaf_names
+    if num > len(filtered_leaves):
+        print("%d pickable nodes" %len(filtered_leaves))
+        return filtered_leaves
     picked = []
+    picked_tax = []
     for i in range(num):
-        r = random.randint(1, len(leaf_names))
-        if leaf_names[r] not in picked:
-            picked.append(leaf_names[r])
+        r = random.randint(1, len(filtered_leaves))
+        if filtered_leaves[r] not in picked:
+            otu = filtered_leaves[r]
+            picked.append(otu)
+            picked_tax.append(map_dict[otu])
     return picked
 
 def create_biom_table(sample_metadata, table_id, data, filename):
     '''
-
     :param sample_metadata:
     :param table_id:
     :param data: dictionary in the form of sample_id:list of otus
@@ -65,7 +70,6 @@ def create_biom_table(sample_metadata, table_id, data, filename):
         otus = otus + value
     otu_ids = list(set(otus)) #row index unique otus
     df = pd.DataFrame(columns=sample_id, index=otu_ids)
-    print(df)
     for sample in sample_id:
         sample_data = np.zeros(len(otu_ids))
         for i, otu in enumerate(otu_ids):
@@ -125,14 +129,13 @@ def get_valid_otu_alternative(otu_file, taxid_file, filename):
     print("size after dropping is %s" % len(merged_df))
     merged_df.to_csv(filename, sep='\t', index=False)
 
-def filter_against_tree(otu_file, tree_file):
+def filter_against_tree(otu_file, nodes):
     '''
     remove nodes in otu_file that are not found on the tree. For the remaining nodes, parse into a dictionary
     :param otu_file:
-    :param tree_file:
+    :param nodes: nodes on a selected tree
     :return: a valid {otu:taxid} dict
     '''
-    (T, l, nodes) = unifrac.parse_tree_file(tree_file)
     df = pd.read_table(otu_file, dtype=str)
     #create dict
     map_dict = df.set_index('otu')['taxid'].to_dict()
@@ -145,6 +148,7 @@ def filter_against_tree(otu_file, tree_file):
     for otu in present:
         filtered_dict[otu] = map_dict[otu]
     print('%d pairs left' %len(filtered_dict))
+    return filtered_dict
 
 def _check_rank_list(id_list):
     '''
@@ -171,5 +175,50 @@ def test_merge():
 
 if __name__ == '__main__':
     os.chdir('data/taxid_otu_conversion')
-    filter_against_tree('otu_with_valid_taxid.txt', '../gg/gg_13_5_otus/trees/99_otus.tree')
+    (T, l, nodes) = unifrac.parse_tree_file('../gg/gg_13_5_otus/trees/99_otus.tree')
+    tree = ete3.TreeNode('../gg/gg_13_5_otus/trees/99_otus.tree', format=1, quoted_node_names=True)
+    otu_tax_dict = filter_against_tree('otu_with_valid_taxid.txt', nodes)
+    pickable_otu = list(otu_tax_dict.keys())
+    node1 = random.choice(tree.get_leaves())
+    node2 = random.choice(tree.get_leaves())
+    node3 = random.choice(tree.get_leaves())
+    print(node1.get_distance(node2))
+    print(node2.get_distance(node3))
+    print(node1.get_distance(node3))
+    comm_an12 = tree.get_common_ancestor(node1, node2)
+    comm_an23 = tree.get_common_ancestor(node2, node3)
+    comm_an13 = tree.get_common_ancestor(node1, node3)
+    env1sam1 = {'env1sam1': pick_otu(comm_an12, otu_tax_dict, 50)}
+    env1sam2 = {'env1sam2': pick_otu(comm_an12, otu_tax_dict, 50)}
+    env1sam3 = {'env1sam3': pick_otu(comm_an12, otu_tax_dict, 50)}
+    env1sam4 = {'env1sam4': pick_otu(comm_an12, otu_tax_dict, 50)}
+    env1sam5 = {'env1sam5': pick_otu(comm_an12, otu_tax_dict, 50)}
+    env2sam1 = {'env2sam1': pick_otu(comm_an23, otu_tax_dict, 50)}
+    env2sam2 = {'env2sam2': pick_otu(comm_an23, otu_tax_dict, 50)}
+    env2sam3 = {'env2sam3': pick_otu(comm_an23, otu_tax_dict, 50)}
+    env2sam4 = {'env2sam4': pick_otu(comm_an23, otu_tax_dict, 50)}
+    env2sam5 = {'env2sam5': pick_otu(comm_an23, otu_tax_dict, 50)}
+    env3sam1 = {'env3sam1': pick_otu(comm_an13, otu_tax_dict, 50)}
+    env3sam2 = {'env3sam2': pick_otu(comm_an13, otu_tax_dict, 50)}
+    env3sam3 = {'env3sam3': pick_otu(comm_an13, otu_tax_dict, 50)}
+    env3sam4 = {'env3sam4': pick_otu(comm_an13, otu_tax_dict, 50)}
+    env3sam5 = {'env3sam5': pick_otu(comm_an13, otu_tax_dict, 50)}
+    data = dict()
+    data.update(env1sam1)
+    data.update(env1sam2)
+    data.update(env1sam3)
+    data.update(env1sam4)
+    data.update(env1sam5)
+    data.update(env2sam1)
+    data.update(env2sam2)
+    data.update(env2sam3)
+    data.update(env2sam4)
+    data.update(env2sam5)
+    data.update(env3sam1)
+    data.update(env3sam2)
+    data.update(env3sam3)
+    data.update(env3sam4)
+    data.update(env3sam5)
+    os.chdir('../gg_test_2')
+    create_biom_table('meta', 'gg_test2', data, 'gg_test2.biom')
     #test_merge()
