d6552ae13eaa00cb81f98640089e5fb438b5e34d
raquellewei <weimaomao_3@hotmail.com>
2021-02-14 17:26:30 -0500

    chaged pick_by_distance function, added get_sorted_distance and test_parallel, setup

--- utils.py
+++ utils.py
@@ -7,6 +7,18 @@ import EMDUnifrac as unifrac
 from biom import Table
 import ete3
 from copy import deepcopy
+from multiprocessing import Pool, Manager, Lock
+import time
+
+def setup():
+    os.chdir('data/abundance_test')
+    (T, l, nodes) = unifrac.parse_tree_file('../gg/gg_13_5_otus/trees/99_otus.tree')
+    # (T, l, nodes) = unifrac.parse_tree_file('99_otus.tree')
+    tree = ete3.TreeNode('../gg/gg_13_5_otus/trees/99_otus.tree', format=1, quoted_node_names=True)
+    # tree = ete3.TreeNode('99_otus.tree', format=1, quoted_node_names=True)
+    otu_tax_dict = filter_against_tree('../taxid_otu_conversion/otu_with_valid_taxid.txt', nodes)
+    pickable_otu = list(otu_tax_dict.keys())
+    return (tree, otu_tax_dict, pickable_otu)
 
 def write_list_to_file(file, lst):
     with open(file, 'w') as f:
@@ -57,18 +69,30 @@ def pick_otu_random(node, map_dict, num):
             picked_tax.append(map_dict[otu])
     return picked, picked_tax
 
-def pick_by_dist(tree, node, map_dict, num, close_enough):
-    if len(close_enough) < num:
-        print("you ask for too much, I only have %d nodes satisfying your requirement" %len(close_enough))
-        return
+def pick_by_dist(tree, node, map_dict, dist, enough):
+    '''
+    return all nodes within distance dist of node node.
+    :param tree:
+    :param node:
+    :param map_dict:
+    :param num:
+    :param close_enough:
+    :return:
+    '''
+    if type(node) == str:
+        node = tree&node
     picked=[]
     picked_tax = []
-    for i in range(num):
-        r = random.randint(1, len(close_enough))
-        if close_enough[r] not in picked:
-            otu = close_enough[r]
-            picked.append(otu)
-            picked_tax.append(map_dict[otu])
+    pickable = list(map_dict.keys())
+    for i in range(len(pickable)):
+        if len(picked) == enough:
+            print("after looking at %d nodes I finally found the miserable amount you asked" %i)
+            break
+        n = pickable[i]
+        if node.get_distance(tree&n) <= dist:
+            picked.append(n)
+            picked_tax.append(map_dict[n])
+            print(len(picked))
     return picked, picked_tax
 
 def pick_for_two(tree, node1, node2, map_dict, num):
@@ -106,8 +130,9 @@ def pick_for_two(tree, node1, node2, map_dict, num):
             print("length of picked_1 is %d" % len(picked_1))
     return picked_1, picked_tax_1, picked_2, picked_tax_2
 
-def create_biom_table(sample_metadata, table_id, data, filename):
+def create_biom_table(sample_metadata, table_id, data, filename, normalize=False):
     '''
+    to be called after obtaining data by calling create_data
     :param sample_metadata:
     :param table_id:
     :param data: dictionary in the form of sample_id:list of otus
@@ -115,22 +140,32 @@ def create_biom_table(sample_metadata, table_id, data, filename):
     '''
     otus = []
     sample_id = [] #column index
+    abundance = dict() #otu:abundance
+
     for key,value in list(data.items()):
         sample_id.append(key)
         otus = otus + value
+        for x, otu in enumerate(value):
+            ab = 1./(2**x)
+            ab = ab + np.random.normal(1)
+            abundance[otu] = ab
     otu_ids = list(set(otus)) #row index unique otus
     df = pd.DataFrame(columns=sample_id, index=otu_ids)
     for sample in sample_id:
         sample_data = np.zeros(len(otu_ids))
         for i, otu in enumerate(otu_ids):
             if otu in list(data[sample]):
-                sample_data[i] = 1
+                sample_data[i] = abundance[otu]
+                print(sample_data[i])
         df[sample] = sample_data #fill by column
     table = Table(data=df.to_numpy(), observation_ids=otu_ids, sample_ids=sample_id, observation_metadata=None,
                   sample_metadata=None, table_id=table_id)
     normed = table.norm(axis='sample', inplace=False)
     with open(filename, "w") as f:
-        normed.to_tsv(direct_io=f)
+        if normalize:
+            normed.to_tsv(direct_io=f)
+        else:
+            table.to_tsv(direct_io=f)
 
 def get_valid_otu(otu_file, taxid_file, filename):
     '''
@@ -164,7 +199,7 @@ def get_valid_otu_alternative(otu_file, taxid_file, filename):
     :param filename:
     :return:
     '''
-    # merge two dataframe by accession
+    # merge two datafame by accessiosn
     otu_acc_df = pd.read_table(otu_file, header=None, dtype=str)
     otu_acc_df.rename(columns={0: "otu", 1: "acc"}, inplace=True)
     acc_taxid_df = pd.read_table(taxid_file, header=None, usecols=[0, 1], dtype=str)
@@ -225,8 +260,8 @@ def search_by_distance(tree, node, map_dict, dist, num):
     pickable_copy = deepcopy(list(map_dict.keys()))
     random.shuffle(pickable_copy)
     while len(match) < num and len(pickable_copy)>0:
-        n = pickable_copy.pop():q
-        if node.get_distance(tree&n) <= dist
+        n = pickable_copy.pop()
+        if node.get_distance(tree&n) <= dist:
             match.append(n)
             print(len(match))
             match_taxid.append(map_dict[n])
@@ -234,7 +269,7 @@ def search_by_distance(tree, node, map_dict, dist, num):
         print("%d pickable nodes within distance %d" %len(match) %dist)
     return match, match_taxid
 
-def create_data(num_sam, tree, dist, num_org, map_dict):
+def create_data(num_sam, tree, dist, num_org, map_dict, enough):
     '''
     return a dictionary consisting of num_env environments, with num_sam samples each
     :param num_env:
@@ -245,17 +280,39 @@ def create_data(num_sam, tree, dist, num_org, map_dict):
     taxid_dict = dict()
     node1 = random.choice(tree.get_leaves())
     node2 = random.choice(tree.get_leaves())
-    while node1.get_distance(node2) < 2.5:
+    while node1.get_distance(node2) < 2.0:
         node1 = random.choice(tree.get_leaves())
         node2 = random.choice(tree.get_leaves())
     print(node1, node2)
     print(node1.get_distance(node2))
+    (otu1, tax1) = pick_by_dist(tree, node1, map_dict, dist, enough)
+    (otu2, tax2) = pick_by_dist(tree, node2, map_dict, dist, enough)
+    if num_org > len(otu1):
+        print("environment 1 does not have enough. Only %d present" %len(otu1))
+        return
+    if num_org > len(otu2):
+        print("environment 1 does not have enough. Only %d present" % len(otu2))
+        return
+    print(len(otu1))
+    print(len(otu2))
     for i in range(num_sam):
         print("creating sample round %d" %i)
+        env1otu = []
+        env1tax = []
+        env2otu = []
+        env2tax = []
+        while len(env1otu) < num_org:
+            r = random.randint(0, len(otu1) - 1)
+            if otu1[r] not in env1otu:
+                env1otu.append(otu1[r])
+                env1tax.append(tax1[r])
+        while len(env2otu) < num_org:
+            r = random.randint(0, len(otu2) - 1)
+            if otu1[r] not in env1otu:
+                env2otu.append(otu2[r])
+                env2tax.append(tax2[r])
         env1_otu_key = "{}{}".format('env1sam', i)
         env2_otu_key = "{}{}".format('env2sam', i)
-        (env1otu, env1tax) = search_by_distance(tree, node1, map_dict, dist, num_org)
-        (env2otu, env2tax) = search_by_distance(tree, node1, map_dict, dist, num_org)
         otu_dict.update({env1_otu_key:env1otu})
         otu_dict.update({env2_otu_key:env2otu})
         taxid_dict.update({env1_otu_key: env1tax})
@@ -264,6 +321,50 @@ def create_data(num_sam, tree, dist, num_org, map_dict):
 
 #def create_metadata(data):
 
+def get_sorted_distance(node, node_list, tree):
+    '''
+    get ranked pairwise distance between the nodes
+    :param node_list:
+    :return:
+    '''
+    # node_dist = dict()
+    # sorted_nodes = []
+    if type(node) is str:
+         node = tree&node
+    # for n in node_list:
+    #     dist = node.get_distance(tree&n)
+    #     node_dist[dist] = n
+    # for key in sorted(node_dist):
+    #     print(key)
+    #     sorted_nodes.append(node_dist[key])
+    node_list = list(map(lambda n: tree&n, node_list))
+    node_list.sort(key=lambda x: node.get_distance(x), reverse=False)
+    return node_list
+
+######################test##########################
+def _fun(n, d):
+    return get_sorted_distance(n, d['node_list'], d['tree'])
+
+def test_parallel():
+    (tree, otu_tax_dict, node_list) = setup()
+    node_list = node_list[:20]
+    manager = Manager()
+    d = manager.dict({'tree': tree, 'node_list': node_list})
+    start1 = time.time()
+    for node in node_list:
+        result = get_sorted_distance(node, node_list, tree)
+    end1 = time.time()
+    print(end1-start1)
+
+    start2 = time.time()
+    pool = Pool(10)
+    for each_node in node_list:
+        result = pool.apply_async(_fun, args=(each_node,d))
+        print(each_node, result.get())
+    pool.close()
+    pool.join()
+    end2 = time.time()
+    print(end2-start2)
 
 def test_merge():
     '''
@@ -279,17 +380,11 @@ def test_merge():
 
 
 if __name__ == '__main__':
-    #os.chdir('data/taxid_otu_conversion')
-    #(T, l, nodes) = unifrac.parse_tree_file('../gg/gg_13_5_otus/trees/99_otus.tree')
-    (T, l, nodes) = unifrac.parse_tree_file('99_otus.tree')
-    #tree = ete3.TreeNode('../gg/gg_13_5_otus/trees/99_otus.tree', format=1, quoted_node_names=True)
-    tree = ete3.TreeNode('99_otus.tree', format=1, quoted_node_names=True)
-    otu_tax_dict = filter_against_tree('otu_with_valid_taxid.txt', nodes)
-    #os.chdir('../gg_test_2')
-    #pickable_otu = list(otu_tax_dict.keys())
-    (data, taxdata) = create_data(25, tree, 1, 200, otu_tax_dict)
-    create_biom_table('meta', 'gg_test', data, 'gg_test.tsv')
+    #(tree, otu_tax_dict, node_list) =  setup()
+    #(data, taxdata) = create_data(1, tree, 0.8, 100, otu_tax_dict, 120)
+    #create_biom_table('meta', 'test_1sample', data, 'test_1sample.tsv', False)
     #only if needs profiles to be created at the same time
-    for key, value in list(taxdata.items()):
-        filename = "{}{}".format(key, '.profile')
-        pf.create_profile(value, 'profiles', filename)
+    #for key, value in list(taxdata.items()):
+    #    filename = "{}{}".format(key, '.profile')
+    #    pf.create_profile(value, 'profiles', filename)
+    test_parallel()
