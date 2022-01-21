4609614c3ff8dc037d0f42a41b0013c2111dd0f8
raquellewei <raquelle70679@gmail.com>
2022-01-21 15:55:34 -0500

    added downloading distance matrix

--- get_data_exp1.sh
+++ get_data_exp1.sh
@@ -7,4 +7,7 @@ wget $url -O $tmp_file
 files=$(grep -oE "<Key>[^<]*gg_13_5/[^<]*</Key>" $tmp_file | sed 's/<Key>\([^<]*\)<\/Key>/\1/' | grep ".gz$")
 
 for f in $files; do  wget -P ./reproducibles/experiment1/ "$url$f"; done
+tar -xvzf ./reproducibles/experiment1/*tar*
+gunzip ./reproducibles/experiment1/*.gz
+wget -P ./reproducibles/experiment1/ https://zenodo.org/record/5885631/files/sorted_distance_complete.txt
 rm $tmp_file
