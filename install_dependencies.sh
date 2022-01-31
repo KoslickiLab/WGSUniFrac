7da28c811448c6eef8af4d58450caa6f693bc6d8
raquellewei <raquelle70679@gmail.com>
2022-01-30 21:14:38 -0500

    changed conda install to pip install to install newer version of biom-format

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,8 +2,8 @@
 
 #install packages
 conda install -y ete3
-conda install -y dendropy
-conda install -y -c bioconda biom-format
+conda install -y -c bioconda dendropy
+pip install bioconda biom-format
 conda install -y seaborn
 conda install -y scikit-bio
 
